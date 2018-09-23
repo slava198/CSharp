7dfbf4e56218626cfd12fe839abdb3964083dd21
Elefuntik <ivdziarzhynski@gmail.com>
2018-09-23 15:53:19 +0300

    Add 2nd project

--- LessonWork2/Program.cs
+++ LessonWork2/Program.cs
@@ -6,10 +6,60 @@ using System.Threading.Tasks;
 
 namespace LessonWork2
 {
-    class Program
-    {
-        static void Main(string[] args)
+   
+       
+
+   
+        enum CommodityType //тип товара
         {
+            FrozenFood, Food, DomesticChemistry,
+            BuildingMaterials, Petrol
         }
-    }
+        enum TransportType //тип транспорта
+        {
+            Semitrailer, Coupling, Refrigerator, OpenSideTruck,
+            FuelTruck
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите число от 1 до 5");
+                int number = Int32.Parse(Console.ReadLine());
+                if (number > 0 && number < 6)
+                {
+                    CommodityType commodity = (CommodityType)
+                    Enum.GetValues(typeof(CommodityType)).
+                    GetValue(number - 1);
+                    TransportType transport = TransportType.
+                    Semitrailer;
+                    switch (commodity)
+                    {
+                        case CommodityType.FrozenFood:
+                            transport = TransportType.Refrigerator;
+                            break;
+                        case CommodityType.Food:
+                          
+ transport = TransportType.Semitrailer;
+ break;
+ case CommodityType.DomesticChemistry:
+ transport = TransportType.Coupling;
+ break;
+ case CommodityType.BuildingMaterials:
+ transport = TransportType.
+ OpenSideTruck;
+ break;
+ case CommodityType.Petrol:
+ transport = TransportType.FuelTruck;
+ break;
+ }
+        Console.WriteLine($"Для товара - {commodity} необходим транспорт - {transport}.");
+ }
+ else
+ {
+ Console.WriteLine("Ошибка ввода");
+ }
+ }
+ }
 }
+  
---
+++ LessonWork2additional/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonWork2additional
+{
+    class Program
+    {
+
+        class Car
+        {
+
+            public string driver;
+            public int currentSpeed;
+
+            public Car(string name = "Michael", int speed = 0)
+            {
+                driver = name;
+                currentSpeed = speed;
+
+            }
+            
+            
+
+
+            public void UpSpeed(int add)
+            {
+                currentSpeed += add;
+
+            }
+
+
+            public void Print()
+            {
+                Console.WriteLine($"Driver {driver} is going with speed {currentSpeed} km/h!");
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            var car = new Car { currentSpeed = 200, driver = "Luis"};
+            
+            
+            for (int i = 0; i < 10; i++)
+            {
+                car.UpSpeed(10);
+                car.Print();
+            }
+            
+        }
+    }
+}
